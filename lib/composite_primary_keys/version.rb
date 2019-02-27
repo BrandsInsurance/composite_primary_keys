3184ec5a56ea2561da3f9b302d44bd4dc56dc3d5
thestelz <stelzera24@gmail.com>
2019-02-27 16:01:30 -0500

    Monkey Patch for SQL Server 2005
    BrandsInsurance/Atlas#709

--- lib/composite_primary_keys/version.rb
+++ lib/composite_primary_keys/version.rb
@@ -3,6 +3,7 @@ module CompositePrimaryKeys
     MAJOR = 11
     MINOR = 1
     TINY  = 0
-    STRING = [MAJOR, MINOR, TINY].join('.')
+    PRE_RELEASE = 'beta1'
+    STRING = [MAJOR, MINOR, TINY, PRE_RELEASE].compact.join('.')
   end
 end
